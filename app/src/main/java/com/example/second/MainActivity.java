d467460de6f77537938eb3002868009605244911
Softwarica College Lab <softwarica@softwarica.com>
2019-03-29 07:54:30 +0545

    MainActivity.java add mamta

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tvCal;
     Random random=new Random;
 
+    mamta
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
